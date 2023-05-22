110037dbd5c828e383d8b6ab98c8b15b863926b0
Oleh Kravets <105140920+olehkravets4991@users.noreply.github.com>
2023-05-22 12:45:23 -0700

    Create added-more-elements.js

---
+++ added-more-elements.js
@@ -0,0 +1,9 @@
+   let $container = $('#container');
+
+    $('#container').append($h1= $('<h1>').text('Hogwarts'))
+
+    $('#container').append($h2= $('<h2>').text('Oleh'))
+    $('#container').append($h3= $('<h3>').text('Gryffindor'))
+    $('#container').append($h4= $('<h4>').text('Ruta').addClass('dog'))
+    $('#container').append($h4= $('<h4>').text('Holly Wand with Unicorn Hair Core'))
+  });
